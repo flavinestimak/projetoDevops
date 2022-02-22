2e76b624f5ececb623cbdae0a59d2f8211930b5c
flavioestimak <flavioestimak@hotmail.com>
2022-02-22 11:26:17 -0300

    Correção no fechamento de algumas linhas do projeto

--- php/src/index.php
+++ php/src/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> 
 
     <title>Projeto Squad Devops - Flavio Estimo Michalani</title>
   </head>
